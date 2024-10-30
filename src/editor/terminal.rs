7eccbe4377bf0e454c3de44d36ccc13db99730fc
michalshy <91937056+michalshy@users.noreply.github.com>
2024-10-30 19:04:04 +0100

    assignment 9 ch4

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,5 +1,7 @@
 use crossterm::cursor::{MoveTo, Hide, Show};
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType};
+use crossterm::terminal::{
+    disable_raw_mode, enable_raw_mode, size, Clear, ClearType, EnterAlternateScreen,
+    LeaveAlternateScreen};
 use std::io::{stdout, Write, Error};
 use crossterm::style::Print;
 use crossterm::{queue, Command};
@@ -24,12 +26,15 @@ pub struct Position {
 pub struct Terminal;
 impl Terminal {
     pub fn terminate() -> Result<(), Error> {
+        Self::leave_alternate_screen()?;
+        Self::show_caret()?;
         Self::execute()?;
         disable_raw_mode()?;
         Ok(())
     }
     pub fn initialize() -> Result<(), Error> {
         enable_raw_mode()?;
+        Self::enter_alternate_screen()?;
         Self::clear_screen()?;
         Self::execute()?;
         Ok(())
@@ -64,12 +69,26 @@ impl Terminal {
         let width = width_u16 as usize;
         Ok(Size { height, width })
     }
+    pub fn enter_alternate_screen() -> Result<(), Error> {
+        Self::queue_command(EnterAlternateScreen)?;
+        Ok(())
+    }
+    pub fn leave_alternate_screen() -> Result<(), Error> {
+        Self::queue_command(LeaveAlternateScreen)?;
+        Ok(())
+    }
     pub fn hide_caret() -> Result<(), Error>{
         Self::queue_command(Hide)?;
         Ok(())
     }
     pub fn show_caret() -> Result<(), Error> {
         Self::queue_command(Show)?;
+        Ok(())
+    }
+    pub fn print_row(row: usize, line_text: &str) -> Result<(), Error> {
+        Self::move_caret_to(Position { row, col:0 })?;
+        Self::clear_line()?;
+        Self::print(line_text)?;
         Ok(())
     }
     pub fn print(string: &str) -> Result<(), Error> {
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-#![warn(clippy::all, clippy::pedantic, clippy::print_stdout, 
-    clippy::arithmetic_side_effects, clippy::as_conversions, clippy::integer_division)]
+#![warn(
+    clippy::all, 
+    clippy::pedantic, 
+    clippy::print_stdout, 
+    clippy::arithmetic_side_effects, 
+    clippy::as_conversions, 
+    clippy::integer_division)]
 use editor::Editor;
 
 mod editor;
 fn main() {
-    Editor::default().run();
+    Editor::new().unwrap().run();
 }
